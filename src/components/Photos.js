b9a7d5051b5faf827c94a723a87c157d8a0f3814
jenniferthorpe <jenniiferthorpe@gmail.com>
2022-11-11 21:57:28 +0100

    add photoviewer

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -3,19 +3,21 @@ import {
   useLocation,
   useNavigate
 } from "react-router-dom";
-import Card from './Card';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+import cn from 'classnames';
 
 import styles from './styles.module.css';
 
 const Photos = () => {
   const [photos, setPhotos] = useState(null);
+  const [viewPhoto, setViewPhoto] = useState(null);
+  const [isPhotoOpen, setIsPhotoOpen] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
     const getPhotos = async () => {
-      const result = await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${location.state.id}`)
+      const result = await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${location?.state?.id}`)
         .then(response => response.json())
         .then(data => data)
         .catch(err => console.error(err)); //CHECK THIS
@@ -26,20 +28,39 @@ const Photos = () => {
     getPhotos();
   }, []);
 
-  console.log('PHOTOS', photos)
+  const handleOnClick = (photo) => {
+    setViewPhoto(photo.url)
+    setIsPhotoOpen(true)
+  };
+
+  const closeOverlay = () => {
+    setIsPhotoOpen(false)
+  };
+
+  const keepOverlay = (event) => {
+    event.preventDefault()
+  };
+
   if (!photos) {
     return null;
   };
-
-  return (//add image component
+  console.log('PHOTOS', photos)
+  return (//add image component //add title component
     <div>
       <h1>{location.state.albumTitle}</h1>
-      <img src='https://via.placeholder.com/150/2edde0' alt="test" />
-      {photos.map((photo) => {
-        { console.log('URL', photo.thumbnailUrl) }
-      })}
+      <div className={styles.thumbnailWrapper}>
+        {photos.map((photo) => (
+          <img src={photo.thumbnailUrl} alt="test" key={photo.id} onClick={() => handleOnClick(photo)} />
+        ))}
+      </div>
+      {isPhotoOpen &&
+        <>
+          <div className={styles.photoViewerWrapper} onClick={closeOverlay}>
+          </div>
+          <img src={viewPhoto} className={styles.photoViewer} alt="test" onClick={keepOverlay} />
+        </>}
     </div>
-  )
+  );
 };
 
 export default Photos;
